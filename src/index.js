777572197002712d2771fef26fce67ce995fbc89
F <19523872+LPink777@users.noreply.github.com>
2018-09-27 17:15:13 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,4 @@
+require("./index.css");
+const show = require("./show.js");
+// 执行 show 函数
+show("Webpack");
--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,11 @@ const glob = require('glob');
 
 const cssExtract = new ExtractTextWebpackPlugin({
     filename: 'css/[name]-one.css',
+    allChunks: true,
 });
 const lessExtract = new ExtractTextWebpackPlugin({
     filename: 'css/[name]-two.css',
+    allChunks: true,
 });
 
 module.exports = {
@@ -29,7 +31,7 @@ module.exports = {
     output: {
         filename: '[name].[hash:7].js',
         //这个路径是绝对路径
-        path: path.resolve('./dist')
+        path: path.resolve(__dirname, './dist')
     },
 
     //开发服务器配置
@@ -38,7 +40,9 @@ module.exports = {
         port: 3000,
         compress: true, //服务器压缩
         open: true, //自动打开浏览器
-        hot: true, //热更新
+        hot: true, //热更新 
+        historyApiFallback: true,
+        inline: true,
     },
 
     //模块配置
@@ -52,7 +56,8 @@ module.exports = {
                     }, {
                         loader: 'postcss-loader', //补全css样式兼容
                     }]
-                })
+                }),
+                exclude: path.resolve(__dirname, 'node_modules')
             }, {
                 test: /\.less$/i,
                 use: lessExtract.extract({
@@ -63,6 +68,21 @@ module.exports = {
                         loader: 'less-loader',
                     }]
                 }),
+                exclude: path.resolve(__dirname, 'node_modules'),
+            }, {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader?cacheDirectory',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: ['@babel/plugin-transform-runtime']
+                    }
+                }
+            }, {
+                // 对非文本文件采用 file-loader 加载
+                test: /\.(gif|png|jpe?g|eot|woff|ttf|svg|pdf)$/,
+                use: ['file-loader'],
             },
 
         ]
@@ -72,20 +92,32 @@ module.exports = {
     plugins: [
         cssExtract,
         lessExtract,
+
         new webpack.HotModuleReplacementPlugin(),
+
         new CleanWebpackPlugin(['./dist']),
+
         new HtmlWebpackPlugin({ //如果是多入口文件，有几个入口文件就new几个htmlplugin
             filename: 'index.html',
             template: './src/index.html',
-            title: 'my-front-end',
+            title: 'webpack4.0',
             hash: true, //引文件得时候后缀加hash
-            // minify: {     removeAttributeQuotes: true, //删除双引号     collapseWhitespace:
-            // true, //折叠空行,变为一行 }
-            // chunks: ['index'], //选择对应得入口文件
+            // minify: {
+            //     //删除双引号   
+            //     removeAttributeQuotes: true,
+            //     //折叠空行,变为一行
+            //     collapseWhitespace: true,
+            // },
+            // //选择对应得入口文件
+            // chunks: ['index'], 
         }),
-        // new HtmlWebpackPlugin({     filename: 'b.html',     template:
-        // './src/index.html',     title: 'my-front-end',     hash: true,     chunks:
-        // ['a'], })
+        // new HtmlWebpackPlugin({
+        //     filename: 'b.html',
+        //     template: './src/index.html',
+        //     title: 'webpack4.0',
+        //     hash: true,
+        //     chunks: ['a'],
+        // })
 
         //消除没有用到得css
         new PurifycssWebpack({
@@ -96,6 +128,21 @@ module.exports = {
     //模式
     mode: 'development',
 
+    // 输出文件性能检查配置
+    performance: {
+        hints: 'warning', // 有性能问题时输出警告
+        maxAssetSize: 200000, // 最大文件大小 (单位 bytes)
+        maxEntrypointSize: 400000, // 最大入口文件大小 (单位 bytes)
+    },
+
     //配置解析
-    resolve: {}
+    resolve: {
+        //替换文件路径
+        alias: {
+            Component: './src/component'
+        },
+        extensions: ['.js', 'jsx', '.json', '.css', '.less']
+    },
+
+    devtool: 'source-map'
 }
