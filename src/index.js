401ba984299b1e2dd0a66ccc6188edf97e5a2f03
F <19523872+LPink777@users.noreply.github.com>
2018-09-28 23:04:47 +0800

    'update'

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import Comp from "./component";
 import ReactDom from "react-dom";
 import './index.css';
+
 class App extends Component {
     render() {
         return (
             <div>
                 <div>test11</div>
-                <div>test22</div>
+                <divs>test22</divs>
                 <Comp/>
             </div>
         );
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const PurifycssWebpack = require('purifycss-webpack'); //打包时去除掉没有用到得css
@@ -10,10 +9,21 @@ const ImageminPlugin = require("imagemin-webpack-plugin").default;
 const glob = require('glob');
 const devMode = process.env.NODE_ENV !== 'production';
 
+const website = {
+    publicPath: 'http://localhost:3000/'
+}
+
 module.exports = {
     // 单页-->index.html 引用了多个js   多页-->  a.html-->index.js || b.html-->a.js 
     // 1.单入口文件得写法
-    entry: './src/index.js',
+    entry: {
+        app: './src/index.js',
+        vendor: [
+            'react',
+            'react-dom',
+            'react-router-dom'
+        ]
+    },
     // 2.数组得写法 
     // entry: ['./src/index.js','./src/a.js'],
     // 3.多入口文件写法 
@@ -24,9 +34,10 @@ module.exports = {
 
     // 出口文件
     output: {
-        filename: '[name].[hash:7].js',
+        filename: '[name].[hash:8].js',
         //这个路径是绝对路径
-        path: path.resolve(__dirname, './dist')
+        path: path.resolve(__dirname, './dist'),
+        // publicPath: website.publicPath
     },
 
     //开发服务器配置
@@ -36,8 +47,8 @@ module.exports = {
         compress: true, //服务器压缩
         open: true, //自动打开浏览器
         hot: true, //热更新 
-        historyApiFallback: true,
-        inline: true,
+        // historyApiFallback: true,
+        // inline: true,
     },
 
     //模块配置
@@ -45,9 +56,9 @@ module.exports = {
         rules: [{
                 test: /\.css$/,
                 use: [
-                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    { loader: MiniCssExtractPlugin.loader },
                     'css-loader',
-                    'postcss-loader',
+                    'postcss-loader'
                 ],
             }, {
                 test: /\.js$/,
@@ -60,11 +71,6 @@ module.exports = {
                         ],
                         plugins: ['transform-runtime', "transform-class-properties"]
                     }
-                }
-            }, {
-                test: /\.(html|htm)/,
-                use: {
-                    loader: 'html-withimg-loader'
                 }
             }, {
                 test: /\.(gif|png|jpe?g|eot|woff|ttf|svg|pdf)$/,
@@ -84,19 +90,15 @@ module.exports = {
 
     //插件配置
     plugins: [
-        new MiniCssExtractPlugin({
-            filename: devMode ? '[name].css' : '[name].[hash].css',
-            chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
-        }),
+        new CleanWebpackPlugin(['./dist']),
 
         new webpack.HotModuleReplacementPlugin(),
 
-        new CleanWebpackPlugin(['./dist']),
 
         new HtmlWebpackPlugin({ //如果是多入口文件，有几个入口文件就new几个htmlplugin
             filename: 'index.html',
             template: './src/index.html',
-            title: 'webpack4.0',
+            title: 'APP',
             hash: true, //引文件得时候后缀加hash
             // minify: {
             //     //删除双引号   
@@ -115,46 +117,52 @@ module.exports = {
         //     chunks: ['a'],
         // })
 
-        //消除没有用到得css
-        new PurifycssWebpack({
-            paths: glob.sync(path.resolve('src/*.html')),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css',
         }),
 
-        // new UglifyJsPlugin({
-        //     // 最紧凑的输出
-        //     beautify: false,
-        //     // 删除所有的注释
-        //     comments: false,
-        //     compress: {
-        //         // 在UglifyJs删除没有用到的代码时不输出警告
-        //         warnings: false,
-        //         // 删除所有的 `console` 语句，可以兼容ie浏览器
-        //         drop_console: true,
-        //         // 内嵌定义了但是只用到一次的变量
-        //         collapse_vars: true,
-        //         // 提取出出现多次但是没有定义成变量去引用的静态值
-        //         reduce_vars: true,
-        //     }
-        // }),
+        // 消除没有用到得css
+        new PurifycssWebpack({
+            paths: glob.sync(path.resolve(__dirname, 'src/*.html')),
+        }),
 
-        //文件拷贝
+        // 文件拷贝
         new CopyWebpackPlugin([{
             from: __dirname + '/src/static',
             to: './static'
         }]),
 
-        //图片压缩
+        // 图片压缩
         new ImageminPlugin({ test: /\.(jpe?g|png|gif|svg)$/i }),
     ],
 
-    //模式
-    mode: 'development',
-
-    // 输出文件性能检查配置
-    performance: {
-        hints: 'warning', // 有性能问题时输出警告
-        maxAssetSize: 200000, // 最大文件大小 (单位 bytes)
-        maxEntrypointSize: 400000, // 最大入口文件大小 (单位 bytes)
+    optimization: {
+        splitChunks: {
+            chunks: "async",
+            minSize: 30000,
+            minChunks: 1,
+            maxAsyncRequests: 5,
+            maxInitialRequests: 3,
+            name: true,
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendor',
+                    chunks: 'all',
+                    priority: 10,
+                    enforce: true
+                },
+                utils: {
+                    test: /[\\/]common[\\/]/,
+                    name: 'common',
+                    chunks: 'all',
+                    minSize: 0,
+                    priority: 10,
+                    enforce: true
+                }
+            }
+        }
     },
 
     //配置解析
@@ -163,8 +171,7 @@ module.exports = {
         alias: {
             '@': path.join(__dirname, './src'),
         },
-        extensions: ['.js', 'jsx', '.json', '.css', '.less'],
-        modules: [path.resolve(__dirname, 'node_modules')]
+        extensions: ['.js', 'jsx', '.json', '.css'],
     },
 
     devtool: 'source-map'
